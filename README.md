05f3adeabd1221858c26a77c961cec22b3d840d0
simonecarolynr <simonecarolynr@gmail.com>
2019-10-13 20:41:51 -0600

    cards-not-done

--- README.md
+++ README.md
@@ -29,15 +29,17 @@ Demonstrate your understanding of this week's concepts by answering the followin
 Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your project manager
 
 1. What is the DOM?
+The document object model (DOM) is the interface that allows developers to manipulate/add code on a website. While it is not pure HTML, script, or styling, it can work with all of these to design and implement robust websites.
 
 2. What is an event?
+An event is something that happens, such as a key being pressed, a mouse moving/clicking, a window changing size; essentially any input into the computer can in some way be an event.
 
 3. What is an event listener?
-
+An event listener is a function that 'listens' or watches for an event to occur, and then executes a function.
 4. Why would we convert a NodeList into an Array?
 
 5. What is a component?
-
+In essence, a component is the melding of HTML, CSS, and Javascript; it is a robust and dynamic piece of a website that gets developers paid.
 ### Git Set up
 
 * [ ] Fork the project into your GitHub user account
--- components/Cards/index.js
+++ components/Cards/index.js
@@ -3,8 +3,7 @@
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
 // Stduy the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
-// This won't be as easy as just iterating over an array though.
-// Create a function that will programmatically create the following DOM component:
+// X Create a function that will programmatically create the following DOM component:
 //
 // <div class="card">
 //   <div class="headline">{Headline of article}</div>
@@ -17,3 +16,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+const articleCards = document.querySelector('.cards-container');
+
+axios
+.get('https://lambda-times-backend.herokuapp.com/articles')
+.then ((response) => {
+    response.data.articles.forEach((topic) => {
+        topic.forEach((arrItem) => {
+            articleCards.appendChild(articleCreator(arrItem))
+        }); 
+    })
+})
+
+.catch((err) => {
+    console.log(err);
+});
+
+
+
+function articleCreator(articleTopic) {
+    const articleCard = document.createElement('div');
+    const articleHeadline = document.createElement('div');
+    const articleAuthor = document.createElement('div');
+    const authorImgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const articleAuthorName = document.createElement('span');
+
+    articleCard.classList.add('card');
+    articleHeadline.classList.add('headline');
+    articleAuthor.classList.add('author');
+    authorImgContainer.classList.add('img-container');
+
+    img.src = articleTopic.authorImg;
+
+    articleHeadline.textContent = articleTopic.headline;
+    articleAuthorName.textContent = `By: ${articleTopic.authorName}`;
+
+    articleCard.appendChild(articleHeadline);
+    articleCard.appendChild(articleAuthor);
+    articleAuthor.appendChild(authorImgContainer);
+    authorImgContainer.appendChild(authorImg);
+    articleAuthor.appendChild(articleAuthorName);
+
+    return articleCard;
+};
+
